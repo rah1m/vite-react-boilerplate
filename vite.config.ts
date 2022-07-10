e1c6c625d0a10323ccfe71e97610ac7c29f1c508
rahimfirudunlu <rahim@workstreams.ai>
2022-07-10 17:27:20 +0400

    vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@domains": path.resolve(__dirname, "./src/domains"),
-      "@components": path.resolve(__dirname, "./src/components"),
+      "@store": resolve(__dirname, "./src/store"),
+      "@domains": resolve(__dirname, "./src/domains"),
+      "@components": resolve(__dirname, "./src/components"),
     },
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
 });
