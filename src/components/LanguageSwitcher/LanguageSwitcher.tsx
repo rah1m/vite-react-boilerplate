8bba4adeaa426694c67457d6ff6f6c15eafb081d
rahimfirudunlu <rahim@workstreams.ai>
2022-07-21 10:39:38 +0400

    add language swithcer

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,6 +1,9 @@
 import { useTranslation } from "react-i18next";
+import { useLocation, useNavigate } from "react-router-dom";
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
   const lngs = {
     en: { nativeName: "English" },
     ru: { nativeName: "Russian" },
@@ -16,7 +19,12 @@ const LanguageSwitcher = () => {
             fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
           }}
           type="submit"
-          onClick={() => i18n.changeLanguage(lng)}
+          onClick={() => {
+            navigate(
+              `${lng}/${location.pathname.split("/").slice(2).join("/")}`
+            );
+            i18n.changeLanguage(lng);
+          }}
         >
           {(lngs as any)[lng].nativeName}
         </button>
